d4840119b0413d171095c3f53b56973629a355e5
jscharpff <jcdscharpff@gmail.com>
2022-11-25 10:15:38 +0100

    Preparing for the first challenge

---
+++ src/challenges/day01/Day01.java
@@ -0,0 +1,23 @@
+package challenges.day01;
+
+import aocutil.io.FileReader;
+
+public class Day01 {
+
+	/**
+	 * Day 1 of the Advent of Code 2021
+	 * 
+	 * https://adventofcode.com/2021/day/1
+	 * 
+	 * @param args The command line arguments
+	 * @throws Exception
+	 */
+	public static void main( final String[] args ) throws Exception {
+		final int[] ex_input = new FileReader( Day01.class.getResource( "day01_example.txt" ) ).readIntArray( );
+		final int[] input = new FileReader( Day01.class.getResource( "day01_input.txt" ) ).readIntArray( );
+		
+		System.out.println( "---[ Part 1 ]---" );
+
+//		System.out.println( "\n---[ Part 2 ]---" );
+	}
+}
